5da53ef48215fec9856822afd69736bd5f084719
Daniel Gendelman <gendeld@gmail.com>
2018-08-01 22:35:04 +0300

    Added some comments

--- src/App.js
+++ src/App.js
@@ -4,74 +4,85 @@ import Sockette from 'sockette';
 import EventView from './eventView.js';
 
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          searchValue: '',
-          events: []
-      };
-      this.eventQueue = [];
-    }
-    addEvent() {
-      if(this.eventQueue.length>0) {
-        let events = this.state.events.slice();
-        let event = this.eventQueue.shift();
-        events.unshift(event);
-        this.setState({ events });
-      }
-    }
-    onMessage(e) {
-      console.log('Received:', e);
-      let event = JSON.parse(e.data);
-      this.eventQueue.unshift(event);
-    }
-    onConnect(e) {
-      console.log('Connected!', e);
-      this.ws.send('Hello World!');
-      setInterval(this.addEvent.bind(this),500);
-    }
-    componentDidMount() {
-      this.ws = new Sockette('ws://events-stream.herokuapp.com/', {
-        timeout: 5e3,
-        maxAttempts: 10,
-        onopen: this.onConnect.bind(this),
-        onmessage: this.onMessage.bind(this),
-        onreconnect: e => console.log('Reconnecting...', e),
-        onmaximum: e => console.log('Stop Attempting!', e),
-        onclose: e => console.log('Closed!', e),
-        onerror: e => console.log('Error:', e)
-      });
-    }
-    handleChange(event) {
-        this.setState({searchValue: event.target.value});
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        searchValue: '',
+        events: []
+    };
+    // Queue events so they won't appear too fast
+    this.eventQueue = [];
+  }
 
-    filterEvents(events, searchValue) {
-        console.warn(`searchValue: ${searchValue}`);
-        let filteredEvents = events.slice();
-        filteredEvents = filteredEvents.filter((event) => {
-            let eventKeys = Object.keys(event);
-            let i = 0;
-            let foundLogo = false;
-            while(!foundLogo&&i<eventKeys.length) {
-                if(eventKeys[i]==="logo") {
-                    foundLogo=true;
-                    eventKeys.splice(i,1);
-                }
-                i++;
+  // On an interval, takes an event from the queue and adds it to the state
+  addEvent() {
+    if(this.eventQueue.length>0) {
+      let events = this.state.events.slice();
+      let event = this.eventQueue.shift();
+      events.unshift(event);
+      this.setState({ events });
+    }
+  }
+
+  // Message received, add event data to queue
+  onMessage(e) {
+    console.log('Received:', e);
+    let event = JSON.parse(e.data);
+    this.eventQueue.unshift(event);
+  }
+
+  // Connected successfully to the WebSocket, send a message in order to start receiving events
+  onConnect(e) {
+    console.log('Connected!', e);
+    this.ws.send('Hello World!');
+    setInterval(this.addEvent.bind(this),500);
+  }
+
+  componentDidMount() {
+    // Sets up connection to WebSocket
+    this.ws = new Sockette('ws://events-stream.herokuapp.com/', {
+      timeout: 5e3,
+      maxAttempts: 10,
+      onopen: this.onConnect.bind(this),
+      onmessage: this.onMessage.bind(this),
+      onreconnect: e => console.log('Reconnecting...', e),
+      onmaximum: e => console.log('Stop Attempting!', e),
+      onclose: e => console.log('Closed!', e),
+      onerror: e => console.log('Error:', e)
+    });
+  }
+
+  // Sets input value to state
+  handleChange(event) {
+      this.setState({searchValue: event.target.value});
+  }
+
+  // Filter events by search input value
+  filterEvents(events, searchValue) {
+    let filteredEvents = events.slice();
+    filteredEvents = filteredEvents.filter((event) => {
+        let eventKeys = Object.keys(event);
+        let i = 0;
+        let foundLogo = false;
+        // Do not filter by logo address
+        while(!foundLogo&&i<eventKeys.length) {
+            if(eventKeys[i]==="logo") {
+                foundLogo=true;
+                eventKeys.splice(i,1);
             }
-            let eventValues = eventKeys.map((eventKey) => {
-                return event[eventKey];
-            });
-
-            console.warn(`eventValues: ${JSON.stringify(eventValues)}`);
-            return eventValues.some((eventValue) => {
-                return eventValue.toString().toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
-            });
+            i++;
+        }
+        let eventValues = eventKeys.map((eventKey) => {
+            return event[eventKey];
         });
-        console.warn(`filteredEvents: ${JSON.stringify(filteredEvents)}`);
-        return filteredEvents;
-    }
+        // If some of the values contain the search input, return true
+        return eventValues.some((eventValue) => {
+            return eventValue.toString().toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+        });
+    });
+    return filteredEvents;
+  }
+
   render() {
     const { events, searchValue } = this.state;
 
--- src/eventValueView.js
+++ src/eventValueView.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import './eventValueView.css';
 
+// Event value, highlights the search value in the event
 class EventValueView extends Component {
+  // Split string by search value, ("Electricity", "ect") => ["El","ect","ricity"]
   splitValue(eventValue, searchValue) {
     let regEx = new RegExp(searchValue,'i');
+     // Splits the string at every point the search value appears, case insensitive
     let strArr = eventValue.split(regEx);
+    /* Adds back the searchValue that was cut out,
+      ends one iteration early so as not to add an extra instance of searchValue */
     for(let i=0; i<(strArr.length-1); i++) {
+      /* If there is an instance of searchValue in the begining or end of the string, it will be "",
+        replace it with searchValue */
       if(strArr[i]==="") {
         strArr[i]=searchValue;
       }
       else {
+        // Add searchValue after item in position i
         strArr.splice(i+1,0,searchValue);
         i++;
       }
     }
+    // See line 14
     if(strArr[strArr.length-1]==="") {
       strArr.pop();
     }
+    // Replace letters to match original casing that might have changed to searchValue casing
     let position = 0;
     for(let j=0; j<strArr.length; j++) {
       let newPosition = position + strArr[j].length;
